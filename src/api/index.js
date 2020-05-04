bd81ede47d238b1d15e9bb420d5d83d002f042bf
matsinvasion <matkmusasizi@gmail.com>
2020-05-04 22:50:54 +0300

    refactor

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const ApiEndpointUrl = 'https://covid19.mathdro.id/api';
 export const fetchData = async () =>{
     try{
-        const response = await axios.get(ApiEndpointUrl);
-        return response
+        const {data: { confirmed, recovered, deaths, lastUpdate}} = await axios.get(ApiEndpointUrl);
+        return { confirmed, recovered, deaths, lastUpdate }
 
     }catch (ex){
 
