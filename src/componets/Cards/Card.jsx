1fb3502baeda232fa353011499704cbfdce78548
matsinvasion <matkmusasizi@gmail.com>
2020-05-04 23:17:58 +0300

    build card

--- src/componets/Cards/Card.jsx
+++ src/componets/Cards/Card.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid} from '@material-ui/core';
+import styles from './Cards.module.css';
 
 //use functional components
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props)
     return ( 
-        <h1>Cards</h1>
+     <div className={styles.container}>
+         <Grid container spacing={3} justify="center">
+             <Grid item component={Card}>
+                 <CardContent>
+                     <Typography color="textSecondary" gutterBottom>
+                         Infected
+                     </Typography>
+                     <Typography variant="h5">
+                         REAL DATA
+                     </Typography>
+                     <Typography color="textSecondary">
+                         REAL DATE
+                     </Typography>
+                     <Typography variant="body2">ACTIVE CASES</Typography>
+                 </CardContent>
+
+             </Grid>
+
+         </Grid>
+
+     </div>
      );
 }
  
