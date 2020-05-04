338757917af16a420d6377fa8020e5575e6d7139
matsinvasion <matkmusasizi@gmail.com>
2020-05-04 22:37:12 +0300

    api functionality

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,13 @@
 import axios from 'axios';
 
-const ApiEndpointUrl = 'https://covid19.mathdro.id/api' 
+const ApiEndpointUrl = 'https://covid19.mathdro.id/api';
+export const fetchData = async () =>{
+    try{
+        const response = await axios.get(ApiEndpointUrl);
+        return response
+
+    }catch (ex){
+
+    }
+
+}
