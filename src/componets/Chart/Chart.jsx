81e39e7ec8c214c60b7820f6a7e3a92abff0d045
matsinvasion <matkmusasizi@gmail.com>
2020-05-05 10:35:39 +0300

    React Hooks

--- src/componets/Chart/Chart.jsx
+++ src/componets/Chart/Chart.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { fetchDailyData } from '../../api';
 
 const Charts = () => {
+    const [dailyData,setDailyData] = useState({});
+    useEffect(() => {
+        const fetchApi = async () => {
+            setDailyData(await fetchDailyData())
+        }
+        fetchApi();
+    })
     return ( 
         <h1>Charts</h1>
      );
