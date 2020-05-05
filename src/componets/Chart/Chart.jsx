6987ea838e00c8a4196dbc927a45d6b9772b70df
matsinvasion <matkmusasizi@gmail.com>
2020-05-05 13:15:30 +0300

    add bar chart for country specific info

--- src/componets/Chart/Chart.jsx
+++ src/componets/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import {Line, Bar} from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Charts = () => {
+const Charts = ({data:{confirmed,recovered,deaths}, country }) => {
     const [dailyData,setDailyData] = useState([]);
     useEffect(() => {
         
@@ -12,7 +12,7 @@ const Charts = () => {
             setDailyData(await fetchDailyData())
         }
         fetchApi();
-    });
+    },[]);
 const lineChart = (
         dailyData.length ? (<Line data={{ labels:dailyData.map(({ date }) => date), datasets:[
             {data: dailyData.map(({ confirmed }) => confirmed),
@@ -25,9 +25,24 @@ const lineChart = (
         backgroundColor:'rgba(255, 0, 0, 0.5)',
         fill:true}]}}/>) : null    
     );
+
+    const barChart = (
+        confirmed
+        ? (
+            <Bar 
+            data={{ labels:['Infected', 'Recovered', 'Deaths'], 
+            datasets:[{label:'people',
+            backgroundColor: ['rgba(0,0,255,0.5)',
+            'rgba(0,255,0,0.5)',
+            'rgba(255,0,0,0.5)'],
+            data:[confirmed.value,recovered.value,deaths.value]
+        }]}} 
+            options={{legend: {display:false},title: {display:true,text:`Current state in ${country}`}}} />
+        ): null
+    )
     return ( 
       <div className={styles.container}>
-          {lineChart}
+          {country ? barChart : lineChart}
 
       </div>
      );
